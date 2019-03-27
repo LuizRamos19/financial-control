648f1c410e06da7addacb404f97374c2f549793b
LuizRamos19 <luizgdramos@gmail.com>
2019-03-26 23:03:35 -0300

    Criando menu mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'header',
-  templateUrl: './header.html',
-  styleUrls: ['./header.scss']
+	selector: 'header',
+	templateUrl: './header.html',
+	styleUrls: ['./header.scss']
 })
 export class HeaderComponent {
-  title = 'financial-control';
+	private show: boolean = false;
+	private menuImagePath: string = 'assets/img/menu.png';
+
+	private showMenu() {
+		this.show = !this.show;
+		this.menuImagePath = this.show ? 'assets/img/close.png' : 'assets/img/menu.png';
+	}
+
 }
